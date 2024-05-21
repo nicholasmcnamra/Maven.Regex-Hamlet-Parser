5beabaf8acf5f0fc5ee653d84ff7f30715c09725
Nicholas McNamara <nikomcnamara@gmail.com>
2024-05-21 19:57:35 -0400

    finished

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -11,13 +13,13 @@ public class HamletParser {
 
     private String hamletData;
 
-    public HamletParser(){
-        this.hamletData = loadFile();
+    public HamletParser(String fileName){
+        this.hamletData = loadFile(fileName);
     }
 
-    private String loadFile(){
+    private String loadFile(String fileName){
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("hamlet.txt").getFile());
+        File file = new File(classLoader.getResource(fileName).getFile());
         StringBuilder result = new StringBuilder("");
 
         try(Scanner scanner = new Scanner(file)){
@@ -56,5 +58,50 @@ public class HamletParser {
             count++;
         }
         return count;
+    }
+
+    public String replaceText(String fullTxt, String textToReplace, String replaceWith) {
+        int count = 0;
+        Pattern p = Pattern.compile("(?<!\\w)" + textToReplace + "(?!\\w)", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(fullTxt);
+        StringBuffer strBuffer = new StringBuffer();
+        while(m.find()) {
+            m.appendReplacement(strBuffer, replaceWith);
+            count++;
+        }
+        System.out.println(fullTxt);
+        return String.format("%d replacements", count);
+    }
+
+    public String writeHamletToNewFile(String fullTxt) throws IOException {
+        int count = 0;
+        int ha = 0;
+        FileWriter writer = new FileWriter("/Users/nicholasmcnamara/dev/labs/Maven.Regex-Hamlet-Parser/src/main/resources/newHT.txt", true);
+        Pattern p = Pattern.compile("(?<!\\w)Hamlet(?!\\w)", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(fullTxt);
+        while (m.find()) {
+            writer.append(fullTxt, count, m.start());
+            writer.append("Leon");
+            count = m.end();
+            ha++;
+        }
+        writer.append(fullTxt.substring(count));
+        return String.format("Hamlet replaced with 'Leon' %d times.", ha);
+    }
+
+    public String writeHoratioToNewFile(String fullTxt) throws IOException {
+        int count = 0;
+        int ho = 0;
+        FileWriter writer = new FileWriter("/Users/nicholasmcnamara/dev/labs/Maven.Regex-Hamlet-Parser/src/main/resources/newHT.txt", true);
+        Pattern p = Pattern.compile("(?<!\\w)Horatio(?!\\w)", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(fullTxt);
+        while (m.find()) {
+            writer.append(fullTxt, count, m.start());
+            writer.append("Tariq");
+            count = m.end();
+            ho++;
+        }
+        writer.append(fullTxt.substring(count));
+        return String.format("Horatio replaced with 'Tariq' %d times", ho);
     }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -2,6 +2,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class HamletParserTest {
@@ -10,35 +12,55 @@ public class HamletParserTest {
 
     @Before
     public void setUp() {
-        this.hamletParser = new HamletParser();
+        this.hamletParser = new HamletParser("hamlet.txt");
         this.hamletText = hamletParser.getHamletData();
     }
 
     @Test
     public void testChangeHamletToLeon() {
+        String expected = String.format("%d replacements", 471);
+        String actual = hamletParser.replaceText(hamletText, "Hamlet", "Leon");
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String expected = String.format("%d replacements", 158);
+        String actual = hamletParser.replaceText(hamletText, "Horatio", "Tariq");
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
-        HamletParser hp = new HamletParser();
+
         int expected = 158;
-        String hamletText = hp.getHamletData();
-        int actual = hp.findHoratio(hamletText);
+        int actual = hamletParser.findHoratio(hamletText);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
-        HamletParser hp = new HamletParser();
         // parse text
         // find each occurrence of Hamlet
         int expected = 471;
-        String hamletText = hp.getHamletData();
-        int actual = hp.findHamlet(hamletText);
+        int actual = hamletParser.findHamlet(hamletText);
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void writeHamletToNewFile() throws IOException {
+        String expected = "Hamlet replaced with 'Leon' 471 times.";
+       String actual =  hamletParser.writeHamletToNewFile(hamletText);
+       Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void writeHoratioToNewFile() throws IOException {
+        HamletParser hamP = new HamletParser("newHT.txt");
+        String expected = "Horatio replaced with 'Tariq' 156 times";
+        String hamletText = hamP.getHamletData();
+        String actual = hamP.writeHoratioToNewFile(hamletText);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
